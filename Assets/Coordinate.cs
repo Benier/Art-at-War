15286f0eccec5943d0acd021d3bf8dd3c08578d4
Tim Wang <timwang_98@hotmail.com>
2016-01-06 02:13:12 -0800

    map generator working

---
+++ Assets/Coordinate.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coordinate {
+
+    float x;
+    float y;
+    float z;
+
+    public Coordinate()
+    {
+        x = 0;
+        y = 0;
+        z = 0;
+    }
+
+    public Coordinate(float posx, float posz)
+    {
+        x = posx;
+        y = 0;
+        z = posz;
+    }
+
+    public float GetX()
+    {
+        return x;
+    }
+
+    public void SetX(float posx)
+    {
+        x = posx;
+    }
+
+    public float GetY()
+    {
+        return y;
+    }
+
+    public void SetY(float posy)
+    {
+        y = posy;
+    }
+
+    public float GetZ()
+    {
+        return z;
+    }
+
+    public void SetZ(float posz)
+    {
+        z = posz;
+    }
+}
---
+++ Assets/MapGenerator.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MapGenerator : MonoBehaviour {
+
+    Random random = new Random();
+
+    static int MAP_WIDTH = 40;
+    static int MAP_LENGTH = 40;
+
+    int num_hills = 10;
+    float y_max = 1;
+    float y_min = 0;
+    float max_radius = 4;
+
+    Dictionary<Coordinate, GameObject> map = new Dictionary<Coordinate, GameObject>();
+    //int[,] map;
+
+    //Dictionary<>
+	// Use this for initialization
+	void Start ()
+    {
+        GenerateMap();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void GenerateMap()
+    {
+        GameObject blockPrefab;
+        //map = new int[MAP_WIDTH, MAP_LENGTH];
+        for(int x = MAP_WIDTH / 2 * -1; x < MAP_WIDTH / 2; x++)
+        {
+            for(int z = MAP_LENGTH / 2 * -1; z < MAP_LENGTH/ 2; z++)
+            {
+                blockPrefab = Instantiate(Resources.Load("MapBlockPrefab", typeof(GameObject))) as GameObject;
+                blockPrefab.transform.position = new Vector3(x, 0, z);
+                map.Add(new Coordinate(x, z), blockPrefab);
+            }
+        }
+
+        int iterations = 0;
+        float radius = max_radius;
+        
+        for (int i = 0; i < num_hills; i++)
+        {
+            float x = Random.Range(MAP_WIDTH / 2 * -1, MAP_WIDTH / 2);
+            float z = Random.Range(MAP_LENGTH / 2 * -1, MAP_LENGTH / 2);
+
+            foreach(KeyValuePair<Coordinate, GameObject> g in map)
+            {
+                float y = Mathf.Pow(radius, 2) - (Mathf.Pow((g.Key.GetX() - x), 2) + Mathf.Pow((g.Key.GetZ() - z), 2));
+                if(y > 0)
+                {
+                    //float ynorm = (y - y_min) / (y_max - y_min);
+                    float ynorm = Mathf.InverseLerp(0, 1, y);
+                    g.Key.SetY(ynorm);
+
+                    g.Value.transform.position += new Vector3(0, g.Key.GetY(), 0);
+                }
+            }
+            //map[x, z] += 1;
+        }
+        
+        return;
+    }
+}
