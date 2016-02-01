b6cd9eea1daf280a1b4d9d947d94c83cd9f97b46
Tim Wang <timwang_98@hotmail.com>
2016-01-31 17:59:29 -0800

    Target detection through collision with bounding sphere implemented.

---
+++ Assets/Unit.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour{
+    float range = 2;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+        if(Input.GetKey(KeyCode.R))
+        {
+            Attack();
+        }
+	}
+
+    Unit(float r)
+    {
+        range = r;
+    }
+
+    bool Attack()
+    {
+        bool hit = false;
+        Collider[] hitColliders = Physics.OverlapSphere(new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 1, gameObject.transform.position.z), range);
+        int i = 0;
+        while(i < hitColliders.Length)
+        {
+            GameObject enemy = hitColliders[i].gameObject;
+            if (enemy.GetComponent<Agent>() != null)
+            {
+                Debug.Log("Ayyyy");
+            }
+            i++;
+        }
+        return hit;
+    }
+}
