1cbdfb69c36ab344080138c1aaed634ca50dc10e
Tim Wang <timwang_98@hotmail.com>
2016-04-01 17:25:04 -0700

    Score multiplier based on tile height. Removed old Q learning stuff.

--- Assets/TextureGenerator.cs
+++ Assets/TextureGenerator.cs
@@ -38,6 +38,8 @@ public class TextureGenerator : MonoBehaviour {
 
     public int correctX;
     public int correctY;
+    float xInterval;
+    float yInterval;
     // Use this for initialization
     void Start () {
         inputBaseTexture = Resources.Load("A_la_Recherche_du_Temps_Perdu_CHARCOAL") as Texture2D;
@@ -66,6 +68,8 @@ public class TextureGenerator : MonoBehaviour {
             }
         }
         mapGen = GameObject.Find("MapGenerator").GetComponent<MapGenerator>();
+        xInterval = inputBaseTexture.width / mapGen.MAP_WIDTH;
+        yInterval = inputBaseTexture.height / mapGen.MAP_LENGTH;
         gameLvl = GameObject.Find("GameLvl1").GetComponent<GameLevel1>();
     }
 	
@@ -97,8 +101,6 @@ public class TextureGenerator : MonoBehaviour {
     public void AddPencilHit(Vector3 pos)
     {
         Vector3 texPos;
-        float xInterval = inputBaseTexture.width / mapGen.MAP_WIDTH;
-        float yInterval = inputBaseTexture.height / mapGen.MAP_LENGTH;
         correctX = (int)((pos.x + mapGen.MAP_WIDTH / 2) * xInterval);//(int)((pos.x * xInterval) + ((mapGen.MAP_WIDTH * xInterval) / 2));
         correctY = (int)((pos.z + mapGen.MAP_LENGTH / 2) * yInterval);//(int)((pos.z * yInterval) + ((mapGen.MAP_LENGTH * yInterval) / 2));
         texPos = new Vector3(correctX, 0, correctY);
@@ -110,8 +112,6 @@ public class TextureGenerator : MonoBehaviour {
     public void AddCharcoalHit(Vector3 pos)
     {
         Vector3 texPos;
-        float xInterval = inputBaseTexture.width / mapGen.MAP_WIDTH;
-        float yInterval = inputBaseTexture.height / mapGen.MAP_LENGTH;
         correctX = (int)((pos.x + mapGen.MAP_WIDTH / 2) * xInterval);//(int)((pos.x * xInterval) + ((mapGen.MAP_WIDTH * xInterval) / 2));
         correctY = (int)((pos.z + mapGen.MAP_LENGTH / 2) * yInterval);//(int)((pos.z * yInterval) + ((mapGen.MAP_LENGTH * yInterval) / 2));
         texPos = new Vector3(correctX, 0, correctY);
@@ -123,8 +123,6 @@ public class TextureGenerator : MonoBehaviour {
     public void AddWaterHit(Vector3 pos)
     {
         Vector3 texPos;
-        float xInterval = inputBaseTexture.width / mapGen.MAP_WIDTH;
-        float yInterval = inputBaseTexture.height / mapGen.MAP_LENGTH;
         correctX = (int)((pos.x + mapGen.MAP_WIDTH / 2) * xInterval);//(int)((pos.x * xInterval) + ((mapGen.MAP_WIDTH * xInterval) / 2));
         correctY = (int)((pos.z + mapGen.MAP_LENGTH / 2) * yInterval);//(int)((pos.z * yInterval) + ((mapGen.MAP_LENGTH * yInterval) / 2));
         texPos = new Vector3(correctX, 0, correctY);
@@ -136,8 +134,6 @@ public class TextureGenerator : MonoBehaviour {
     public void AddOilHit(Vector3 pos)
     {
         Vector3 texPos;
-        float xInterval = inputBaseTexture.width / mapGen.MAP_WIDTH;
-        float yInterval = inputBaseTexture.height / mapGen.MAP_LENGTH;
         correctX = (int)((pos.x + mapGen.MAP_WIDTH / 2) * xInterval);//(int)((pos.x * xInterval) + ((mapGen.MAP_WIDTH * xInterval) / 2));
         correctY = (int)((pos.z + mapGen.MAP_LENGTH / 2) * yInterval);//(int)((pos.z * yInterval) + ((mapGen.MAP_LENGTH * yInterval) / 2));
         texPos = new Vector3(correctX, 0, correctY);
@@ -149,14 +145,22 @@ public class TextureGenerator : MonoBehaviour {
     public void AddTarHit(Vector3 pos)
     {
         Vector3 texPos;
-        float xInterval = inputBaseTexture.width / mapGen.MAP_WIDTH;
-        float yInterval = inputBaseTexture.height / mapGen.MAP_LENGTH;
         correctX = (int)((pos.x + mapGen.MAP_WIDTH / 2) * xInterval);//(int)((pos.x * xInterval) + ((mapGen.MAP_WIDTH * xInterval) / 2));
         correctY = (int)((pos.z + mapGen.MAP_LENGTH / 2) * yInterval);//(int)((pos.z * yInterval) + ((mapGen.MAP_LENGTH * yInterval) / 2));
         texPos = new Vector3(correctX, 0, correctY);
 
         TextureHit pHit = new TextureHit(texPos, tarBaseTexture, tarMaskTexture, (int)Faction.None);
         hitQueue.Add(pHit);
+    }
+
+    public Vector3 ConvertToWorldCoord(Vector3 pos)
+    {
+        Vector3 worldPos;
+        int worldX = (int)((pos.x / xInterval) - mapGen.MAP_WIDTH / 2);
+        int worldZ = (int)((pos.z / yInterval) - mapGen.MAP_LENGTH / 2);
+        worldPos = new Vector3(worldX, 0, worldZ);
+
+        return worldPos;
     }
 
     public Texture2D GeneratePass(TextureHit hit, Texture2D outputText)
@@ -175,34 +179,37 @@ public class TextureGenerator : MonoBehaviour {
                     {
                         Color pixel = new Color(hit.baseTexture.GetPixel((int)(hit.position.x - (hit.maskTexture.width / 2) + x), (int)(hit.position.z - (hit.maskTexture.height / 2) + y)).r, hit.baseTexture.GetPixel((int)(hit.position.x - (hit.maskTexture.width / 2) + x), (int)(hit.position.z - (hit.maskTexture.height / 2) + y)).g, hit.baseTexture.GetPixel((int)(hit.position.x - (hit.maskTexture.width / 2) + x), (int)(hit.position.z - (hit.maskTexture.height / 2) + y)).b, 1);//hit.maskTexture.GetPixel(x, y).a);
                         tempTexture.SetPixel((int)(hit.position.x - (hit.maskTexture.width / 2) + x), (int)(hit.position.z - (hit.maskTexture.height / 2) + y), pixel);
-                        
+                        Vector3 worldPos = ConvertToWorldCoord(new Vector3((int)(hit.position.x - (hit.maskTexture.width / 2) + x), 0, (int)(hit.position.z - (hit.maskTexture.height / 2) + y)));
+
                         if (pixels[(int)(hit.position.x - (hit.maskTexture.width / 2) + x), (int)(hit.position.z - (hit.maskTexture.height / 2) + y)].faction == (int)Faction.None)
                         {
-                            if(hit.faction == (int)Faction.Player)
+                            
+                            if (hit.faction == (int)Faction.Player)
                             {
-                                gameLvl.playerPoints++;
+                                
+                                gameLvl.playerPoints += 1 * (int)(mapGen.map[new Coordinate(worldPos.x, worldPos.z)].transform.position.y + 1);
                             }
                             else if(hit.faction == (int)Faction.Enemy)
                             {
-                                gameLvl.enemyPoints++;
+                                gameLvl.enemyPoints += 1 * (int)(mapGen.map[new Coordinate(worldPos.x, worldPos.z)].transform.position.y + 1);
                             }
                         }
                         else if(pixels[(int)(hit.position.x - (hit.maskTexture.width / 2) + x), (int)(hit.position.z - (hit.maskTexture.height / 2) + y)].faction != hit.faction)
                         {
                             if (pixels[(int)(hit.position.x - (hit.maskTexture.width / 2) + x), (int)(hit.position.z - (hit.maskTexture.height / 2) + y)].faction == (int)Faction.Player)
                             {
-                                gameLvl.playerPoints--;
+                                gameLvl.playerPoints -= 1 * (int)(mapGen.map[new Coordinate(worldPos.x, worldPos.z)].transform.position.y + 1);
                                 if(hit.faction != (int)Faction.None)
                                 {
-                                    gameLvl.enemyPoints++;
+                                    gameLvl.enemyPoints += 1 * (int)(mapGen.map[new Coordinate(worldPos.x, worldPos.z)].transform.position.y + 1);
                                 }
                             }
                             else
                             {
-                                gameLvl.enemyPoints--;
+                                gameLvl.enemyPoints -= 1 * (int)(mapGen.map[new Coordinate(worldPos.x, worldPos.z)].transform.position.y + 1);
                                 if (hit.faction != (int)Faction.None)
                                 {
-                                    gameLvl.playerPoints++;
+                                    gameLvl.playerPoints += 1 * (int)(mapGen.map[new Coordinate(worldPos.x, worldPos.z)].transform.position.y + 1);
                                 }
                             }
                         }
